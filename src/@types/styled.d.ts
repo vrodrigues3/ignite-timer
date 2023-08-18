55d9b8266a9fda437fe6b4957e97bb5a1d095865
vrodrigues3 <vrodrigues@tractian.com>
2023-08-18 09:07:39 -0300

    chore: project settings

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,8 @@
+import 'style-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
